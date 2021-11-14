54b58afade951415dc4aaf1279ef8fa8539484a1
AlxDmk <alxdmk@gmail.com>
2021-11-14 21:42:38 +0300

    ДЗ к Уроку 2

---
+++ Lesson2/AccountInBank_V1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2.Lesson2
+{
+    
+    class AccountInBank_V1
+    {
+        private int _accountId;
+        private decimal _balance;
+        private AccountType _accountType;
+
+        public int AccountID
+        {
+            get { return _accountId; }
+            set { _accountId = value; }
+        }
+        public decimal Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+
+        public AccountType TypeOfAccount
+        {
+            get { return _accountType; }
+            set { _accountType = value; }
+        }
+
+    }
+}
